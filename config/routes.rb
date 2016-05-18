76d2c4515f2e127ed7064d417c93728b91df55cd
Thien Vu <tvu@linfield.edu>
2016-05-18 13:21:08 -0700

    Change homepage to static page home

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
   #root 'application#hello'
-  root 'users#index'
+  #root 'users#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
