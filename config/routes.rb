6d6ed65a13594cc3820a95d1c06a6e7491599beb
Thien Vu <tvu@linfield.edu>
2016-05-20 15:37:40 -0700

    change to normal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #get 'sessions/new'
 
-  #root 'static_pages#home'
+  root 'static_pages#home'
   #get 'users/new'
 
   #get 'static_pages/home'
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
-  root 'application#hello'
+  #root 'application#hello'
   #root 'users#index'
   
   
