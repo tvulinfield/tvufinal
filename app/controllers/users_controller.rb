c009f6652c8eaa9b57624353d49f69f119e10ae7
Thien Vu <tvu@linfield.edu>
2016-05-20 14:09:15 -0700

    finish login out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the CollegeNet!"
       redirect_to @user
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518215103) do
+ActiveRecord::Schema.define(version: 20160520205439) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160518215103) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
