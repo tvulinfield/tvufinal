f01d28862e23ecbd1d5c1bd96e9f6b4ee31e55aa
Thien Vu <tvu@linfield.edu>
2016-05-20 13:16:27 -0700

    fuck

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+
   root 'static_pages#home'
   #get 'users/new'
 
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
 
   resources :users
