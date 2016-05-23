3a4b05e70b1a1afe0c7de075d6b3cd1124d3d194
Thien Vu <tvu@linfield.edu>
2016-05-23 13:01:44 -0700

    fix users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,10 +3,9 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-<<<<<<< HEAD
+
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-=======
->>>>>>> 716331d71dde278e0649d55e742667f7a3f35571
+
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
